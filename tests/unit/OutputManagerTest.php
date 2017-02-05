40acc03b8d5b83bdd3ee0cf2f4367785bb95aa52
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2017-02-05 12:11:55 +0100

    add Manager class unit test class

---
+++ tests/unit/OutputManagerTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace SlaxWeb\Output\Tests\Unit;
+
+class OutputManagerTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+}
