46516dc5e37ffefd0a423ebdead8225a816f992e
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2017-02-06 15:29:09 +0100

    add manager constructor to parse config and set deps

--- src/Manager.php
+++ src/Manager.php
@@ -1,6 +1,10 @@
 <?php
 namespace SlaxWeb\Output;
 
+use SlaxWeb\Router\Response;
+use Psr\Log\LoggerInterface as Logger;
+use SlaxWeb\Config\Container as Config;
+
 /**
  * Output Manager
  *
@@ -24,4 +28,69 @@ class Manager
     const MODE_VIEW = 0;
     const MODE_JSON = 1;
     const MODE_FILE = 2;
+
+    /**
+     * Logger instance
+     *
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $logger = null;
+
+    /**
+     * Response object
+     *
+     * @var \SlaxWeb\Router\Response
+     */
+    protected $response = null;
+
+    /**
+     * Enabled
+     *
+     * @var bool
+     */
+    protected $enabled = false;
+
+    /**
+     * Permit direct output
+     *
+     * @var bool
+     */
+    protected $allowOutput = true;
+
+    /**
+     * Output mode
+     *
+     * @var int
+     */
+    protected $mode = 1;
+
+    /**
+     * Permit mode change
+     *
+     * @var bool
+     */
+    protected $allowModeChange = true;
+
+    /**
+     * Class constructor
+     *
+     * Copy dependencies to protected class properties, and parse the configuration
+     * array.
+     *
+     * @param \Psr\Log\LoggerInterface $logger Logger instance
+     * @param \SlaxWeb\Router\Response $response Response object
+     * @param array $settings Output manager settings array
+     */
+    public function __construct(Logger $logger, Response $response, array $settings)
+    {
+        $this->logger = $logger;
+        $this->response = $response;
+
+        $this->enabled = $settings["enabled"] ?? false;
+        $this->allowOutput = $settings["allowOutput"] ?? true;
+        $this->mode = $settings["mode"] ?? self::MODE_JSON;
+        $this->allowModeChange = $settings["allowModeChange"] ?? true;
+
+        $this->logger->info("Output manager initialized");
+    }
 }
