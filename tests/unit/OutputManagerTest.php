4867c996229b7c7f923ea2fd5ac612509ed1d09a
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2017-02-11 11:35:30 +0100

    create temp template files for error handling test

--- tests/unit/OutputManagerTest.php
+++ tests/unit/OutputManagerTest.php
@@ -17,17 +17,61 @@ class OutputManagerTest extends \Codeception\Test\Unit
      */
     protected $response = null;
 
-    protected function _before()
-    {
-        $this->response = Mockery::mock(\SlaxWeb\Router\Response::class);
-    }
+    /**
+     * Temp error template name
+     *
+     * @var string
+     */
+    protected $tplName = "";
 
-    protected function _after()
-    {
-    }
+    /**
+     * Temp error style template name
+     *
+     * @var string
+     */
+    protected $styleTplName = "";
 
     public function testErrorHandling()
     {
 
+    }
+
+    protected function _before()
+    {
+        $this->response = Mockery::mock(\SlaxWeb\Router\Response::class);
+
+        $this->createErrorTemplates();
+    }
+
+    protected function _after()
+    {
+        $this->removeErrorTemplates();
+    }
+
+    protected function createErrorTemplates()
+    {
+        if ($this->tplName !== "" && $this->styleTplName !== "") {
+            // templates exist, re-use
+            return;
+        }
+
+        $hash = sha1(time());
+        $this->tplName = realpath(__DIR__) . "/errorTemplate_{$hash}.php";
+        $this->styleTplName = realpath(__DIR__) . "/styleTemplate_{$hash}.html";
+
+        file_put_contents($this->tplName, "<?=\$severity;?><?=\$message;?><?=\$file;?><?=\$line;?>");
+        file_put_contents($this->styleTplName, "style template\n");
+    }
+
+    protected function removeErrorTemplates()
+    {
+        if (file_exists($this->tplName)) {
+            unlink($this->tplName);
+        }
+        if (file_exists($this->styleTplName)) {
+            unlink($this->styleTplName);
+        }
+        $this->tplName = "";
+        $this->styleTplName = "";
     }
 }
